f38563719e956823867d43d6f68611091c6abc1b
MatheusMendes <50601403+mm490514@users.noreply.github.com>
2024-08-03 13:50:44 -0300

    feat: Adicionando funcionalidades

---
+++ src/controllers/autorController.js
@@ -0,0 +1,55 @@
+import { autor } from "../models/Autor.js";
+
+class AutorController {
+
+    static async listarAutores(req, res) {
+        try {
+            const listaAutores = await autor.find({});
+            res.status(200).json(listaAutores);
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na requisição`});
+        }
+    }
+
+    static async listarAutorPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const autorEncontrado = await autor.findById({id});
+            res.status(200).json(autorEncontrado);
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na requisição do autor`});
+        }
+    }
+
+    static async cadastrarAutor(req, res) {
+        try {
+            const novoAutor = await autor.create(req.body);
+            res.status(201).json({message: "Criado com sucesso", autor: novoAutor});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha ao cadastrar autor`});
+        }
+    }
+
+    static async atualizarautor(req, res) {
+        try {
+            const id = req.params.id;
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "autor atualizado"});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na atualização do autor`});
+        }
+    }
+
+    static async deletarautor(req, res) {
+        try {
+            const id = req.params.id;
+            await autor.findByIdAndDelete(id, req.body);
+            res.status(200).json({message: "autor deletado"});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha ao deletar o autor`});
+        }
+    }
+
+};
+
+export default AutorController;
